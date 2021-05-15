9066a9fd3d3d8c1b886798348ecff63ab82a297d
Tennana <tennana@nifty.com>
2021-05-15 14:26:56 +0900

    fix: Pillowの依存モジュールを追加

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
 sudo apt-get install python3-dev python3-pip libfreetype6-dev libjpeg-dev build-essential
+sudo apt-get install python-pil python3-pil.imagetk
+# sudp apt-get install python-imaging
 sudo -H pip3 install --upgrade luma.oled Pillow gpiozero
