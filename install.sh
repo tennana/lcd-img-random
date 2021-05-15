167dce7273805d0d372543953dfacff36cea53f9
Tennana <tennana@nifty.com>
2021-05-15 15:34:49 +0900

    fix:  libopenjp2-7-devなかった

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -y python3-dev python3-pip libfreetype6-dev libjpeg-dev build-essential
+sudo apt-get install -y python3-dev python3-pip build-essential libopenjp2-7-dev
 sudo apt-get install -y python-pil python3-pil.imagetk
 # sudp apt-get install python-imaging
 sudo -H pip3 install --upgrade luma.oled Pillow gpiozero
